74393d24d4fd8b3c1e7771461d6a6979993da5cc
CoryVegan <Cory.WANG@outlook.com>
2017-08-12 15:51:11 +1000

    A simple example
    To show how to create a repository in GitHub.

---
+++ MainTest.m
@@ -0,0 +1,11 @@
+%% This is the file to test how GitHub works.
+%   1. GitHub is not a Chinese friendly platform, so it's suggested to use
+%   English during developing.
+
+
+%%
+
+vXData  =   [1,2,3,4,5];
+vYData  =   exp(vXData);
+plot(vXData , vYData)
+
